1a80c3bdc8bb2178be8a57664feed0dbfbdfedce
Patilsiddh <151660311+Patilsiddh@users.noreply.github.com>
2024-01-27 10:36:40 +0530

    Number Game

--- Main.java
+++ Main.java
@@ -1,157 +1,70 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static {
-
-        System.out.println("Automated Teller Machine");
-
-    }
-
     public static void main(String[] args) {
-        Bank b = new Bank();
-        b.setup();
-    }
-}
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int number = random.nextInt(101);
 
-class AtmInvalidPinOrAcc extends Exception {
-    @Override
-    public String getMessage() {
-        return "Invalid Card details! Try Again.";
-    }
-}
+        int rounds = 0;
+        int attempts = 2;
+        int score = 0;
 
-class InvalidOperation extends Exception {
-    @Override
-    public String getMessage() {
-        return "Invalid operation performed! Try Again.";
-    }
-}
+        System.out.println("**Welcome to Number Game**");
 
-class AtmCardBlock extends Exception {
-    @Override
-    public String getMessage() {
-        return "Your Card  isBlocked!. Please try contack Bank";
-    }
-}
+        boolean play = true;
+        while (play != false) {
+            rounds++;
 
-class ATM {
-    private int inAccNum;
-    private int inPsw;
-    Scanner sc = new Scanner(System.in);
+            while (attempts != 0) {
+                System.out.print("Enter your guess 1 to 100: ");
+                int userNum = sc.nextInt();
+                if (userNum < 0 || userNum > 100) {
+                    System.out.println("number out of range!");
 
-    public void acceptIN() {
+                    continue;
+                }
 
-        System.out.println("Enter the Account Number");
-        inAccNum = sc.nextInt();
+                if (number == userNum) {
 
-        System.out.println("Enter your pin");
-        inPsw = sc.nextInt();
-    }
+                    System.out.println("Great you Guessed right number!");
+                    if (attempts == 10) {
+                        score += 10;
+                    } else if (attempts >= 5) {
+                        score += 5;
+                    } else {
+                        score += 2;
+                    }
+                    break;
+                } else if (userNum < number) {
 
-    public void validateIn() throws AtmInvalidPinOrAcc, InvalidOperation {
-        boolean valid = true;
-        Bank bank = new Bank();
-        if (inAccNum == bank.getAccNum() && inPsw == bank.getPsw()) {
-            while (valid) {
+                    System.out.println("Enter a Higher number");
+                    attempts--;
+                } else if (userNum > number) {
 
-                System.out.println(
-                        "1.check Balance\n2.Withdraw\n3.Deposite\n4.Logout\n5.Operation to be performed(1|2|3|4):-");
-                int choice = sc.nextInt();
+                    System.out.println("Enter a Lower number");
+                    attempts--;
+                }
 
-                switch (choice) {
-                    case 1:
-                        System.out.println("Available balance in your account XXXX XXXX " + bank.getAccNum()
-                                + " is " + bank.getBalance());
-                        break;
-                    case 2:
+                if (attempts == 0) {
 
-                        System.out.println("Enter the amount to withdraw:");
-                        double amount = sc.nextDouble();
-                        if (amount >= bank.getBalance()) {
-                            System.out.println("insufficient balance!");
-                            break;
-                        } else {
-                            bank.setBalance(bank.getBalance() - amount);
-                            System.out.println("Collect your money: " + amount);
-                            System.out.println("Available balance in your account XXXX XXXX " + bank.getAccNum()
-                                    + " is " + bank.getBalance());
-                            break;
-                        }
-                    case 3:
+                    System.out.println("No attempts left!, the number to be guessed is " + number);
 
-                        System.out.println("Enter the amount to be deposited:");
-                        double Amount = sc.nextDouble();
-                        bank.setBalance(bank.getBalance() + Amount);
-                        System.out.println("Deposited " + Amount + " to your account XXXX XXXX " + bank.getAccNum());
-                        System.out.println("Available balance: " + bank.getBalance());
-                        break;
-                    case 4:
-                        valid = false;
-                        break;
-                    default:
-                        InvalidOperation inv = new InvalidOperation();
-                        System.out.println(inv.getMessage());
                 }
             }
-        } else {
-            AtmInvalidPinOrAcc ex = new AtmInvalidPinOrAcc();
 
-            System.out.println(ex.getMessage());
-            throw ex;
-        }
-    }
-}
+            System.out.print("want to try again?[1(yes)|0(no)]: ");
+            int wantsToPlay = sc.nextInt();
+            if (wantsToPlay != 1) {
 
-class Bank {
-    private int AccNum = 1234;
-    private int psw = 1234;
-    private double balance = 0;
+                System.out.println("you have played " + rounds + " round, your score is " + score);
 
-    public int getAccNum() {
-        return AccNum;
-    }
 
-    public void setAccNum(int accNum) {
-        AccNum = accNum;
-    }
-
-    public int getPsw() {
-        return psw;
-    }
-
-    public void setPsw(int psw) {
-        this.psw = psw;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public void setup() {
-        ATM a = new ATM();
-        try {
-            System.out.println("| please provide credentials to perform actions |");
-            a.acceptIN();
-            a.validateIn();
-        } catch (Exception e) {
-            try {
-                System.out.println("please provide valid credentials to perform actions");
-                a.acceptIN();
-                a.validateIn();
-            } catch (Exception f) {
-                try {
-                    System.out.println("please provide valid credentials to perform actions");
-                    a.acceptIN();
-                    a.validateIn();
-                } catch (Exception g) {
-                    AtmCardBlock ex = new AtmCardBlock();
-                    System.err.println(ex.getMessage());
-                }
+                play = false;
             }
         }
+        sc.close();
     }
+
 }
